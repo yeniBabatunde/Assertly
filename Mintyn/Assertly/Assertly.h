a8593ede0d8fce6a27a7866f907166673558a1b3
yeni_bababatunde <babatundeabike3@gmail.com>
2024-06-22 14:48:12 +0100

    commit_setup

---
+++ Mintyn/Assertly/Assertly.h
@@ -0,0 +1,18 @@
+//
+//  Assertly.h
+//  Assertly
+//
+//  Created by Sharon Omoyeni Babatunde on 12/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Assertly.
+FOUNDATION_EXPORT double AssertlyVersionNumber;
+
+//! Project version string for Assertly.
+FOUNDATION_EXPORT const unsigned char AssertlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Assertly/PublicHeader.h>
+
+
