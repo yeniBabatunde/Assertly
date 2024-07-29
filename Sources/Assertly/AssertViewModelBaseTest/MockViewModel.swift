2ff698315f81e28a852c83c0884578b27607c855
yeni_bababatunde <155441386+omoyeni-babatunde-iOS-fcmb@users.noreply.github.com>
2024-07-29 21:41:51 +0100

    Update MockViewModel.swift

--- Sources/Assertly/AssertViewModelBaseTest/MockViewModel.swift
+++ Sources/Assertly/AssertViewModelBaseTest/MockViewModel.swift
@@ -13,6 +13,8 @@ open class MockViewModel: ViewModelProtocol {
     public var mockResults: [String: Any] = [:]
     public var mockErrors: [String: Error] = [:]
     
+    public init() {}
+    
     public func performAction<T>(_ action: String, completion: @escaping (Result<T, Error>) -> Void) {
         if let error = mockErrors[action] {
             completion(.failure(error))
